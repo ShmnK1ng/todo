ac126873bfc57d17c8cf1cf6297612290e3d74fb
Andrei Shoka <xrdsby@gmail.com>
2022-07-19 16:22:50 +0300

    add add todoNote feature

--- app/src/main/java/com/example/myapplication/TodoListActivity.java
+++ app/src/main/java/com/example/myapplication/TodoListActivity.java
@@ -10,7 +10,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -18,17 +17,35 @@ import java.util.UUID;
 public class TodoListActivity extends AppCompatActivity {
 
     static final String EXTRA_TODO = "EXTRA_TODO";
+    static final String REMOVE_TODO = "REMOVE_TODO";
     ArrayList<Todo> todoList = new ArrayList<>();
     TodoAdapter todoAdapter;
+    private int itemPosition;
+    Todo todoEnteredText;
 
 
     ActivityResultLauncher<Intent> getNote = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             result -> {
 
                 if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                    Todo todoEnteredText = result.getData().getParcelableExtra(EXTRA_TODO);
-                    int itemPosition = todoList.indexOf(todoEnteredText);
+                    todoEnteredText = result.getData().getParcelableExtra(EXTRA_TODO);
+                    itemPosition = todoList.indexOf(todoEnteredText);
                     todoList.set(itemPosition, todoEnteredText);
+                    todoAdapter.notifyItemChanged(itemPosition);
+                }
+            });
+    ActivityResultLauncher<Intent> addNote = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    todoEnteredText = result.getData().getParcelableExtra(EXTRA_TODO);
+                    itemPosition = todoList.indexOf(todoEnteredText);
+                    todoList.set(itemPosition, todoEnteredText);
+                    todoAdapter.notifyItemChanged(itemPosition);
+                }
+                if (result.getResultCode() == Activity.RESULT_CANCELED && result.getData() != null) {
+                    todoEnteredText = result.getData().getParcelableExtra(REMOVE_TODO);
+                    itemPosition = todoList.indexOf(todoEnteredText);
+                    todoList.remove(itemPosition);
                     todoAdapter.notifyItemChanged(itemPosition);
                 }
             });
@@ -41,6 +58,8 @@ public class TodoListActivity extends AppCompatActivity {
         todoList.add(new Todo(UUID.randomUUID().toString(), "second"));
         todoList.add(new Todo(UUID.randomUUID().toString(), "third"));
         initRecyclerView();
+        addButtonClickListener();
+
     }
 
     TodoAdapter.OnTodoItemClickListener todoClickListener = todo -> {
@@ -55,6 +74,16 @@ public class TodoListActivity extends AppCompatActivity {
         todoList_RecyclerView.setLayoutManager(layoutManager);
         this.todoAdapter = new TodoAdapter(todoClickListener, todoList);
         todoList_RecyclerView.setAdapter(todoAdapter);
+    }
+
+    public void addButtonClickListener() {
+        findViewById(R.id.activity_todo_list_add_note_button).setOnClickListener(view -> {
+            Intent startTodoTextNoteActivity = new Intent(this, TodoTextNoteActivity.class);
+            todoList.add(new Todo(UUID.randomUUID().toString(), ""));
+            Todo newTodo = todoList.get(todoList.size() - 1);
+            startTodoTextNoteActivity.putExtra(EXTRA_TODO, newTodo);
+            addNote.launch(startTodoTextNoteActivity);
+        });
     }
 
 }
--- app/src/main/java/com/example/myapplication/TodoTextNoteActivity.java
+++ app/src/main/java/com/example/myapplication/TodoTextNoteActivity.java
@@ -1,10 +1,12 @@
 package com.example.myapplication;
 
 import static com.example.myapplication.TodoListActivity.EXTRA_TODO;
+import static com.example.myapplication.TodoListActivity.REMOVE_TODO;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -20,9 +22,11 @@ public class TodoTextNoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_text_note);
         this.editText = findViewById(R.id.activity_todo_text_note_edit_text);
+        getIntent().getParcelableExtra(EXTRA_TODO);
         this.todo = getIntent().getParcelableExtra(EXTRA_TODO);
         editText.setText(todo.getTodoText());
         addOnButtonClickListener();
+        addNavigationClickListener();
     }
 
     private void sendTextItem() {
@@ -33,15 +37,29 @@ public class TodoTextNoteActivity extends AppCompatActivity {
     }
 
     private void addOnButtonClickListener() {
-
         ((Toolbar) findViewById(R.id.activity_todo_text_note_toolbar)).setOnMenuItemClickListener(item -> {
             String textEntered = editText.getText().toString();
-            todo.setTodoText(textEntered);
-            sendTextItem();
+            if (textEntered.length() == 0) {
+                AlertDialog alertDialog = new AlertDialog.Builder(TodoTextNoteActivity.this).create();
+                alertDialog.setTitle("Error");
+                alertDialog.setMessage("Enter note text");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok",
+                        (dialogInterface, i) -> dialogInterface.dismiss()
+                );
+                alertDialog.show();
+            }else {
+                todo.setTodoText(textEntered);
+                sendTextItem();
+            }
             return true;
-
         });
-
     }
-
+    private void addNavigationClickListener() {
+        ((Toolbar) findViewById(R.id.activity_todo_text_note_toolbar)).setNavigationOnClickListener(item -> {
+            Intent backToTodoListActivity = new Intent(this, TodoListActivity.class);
+            backToTodoListActivity.putExtra(REMOVE_TODO, todo);
+            setResult(RESULT_CANCELED, backToTodoListActivity);
+            finish();
+        });
+    }
 }
