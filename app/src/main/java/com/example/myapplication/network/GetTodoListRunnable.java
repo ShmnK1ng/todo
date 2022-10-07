5248e8b361f09eef9dad37ff56a002612a5c14e3
Andrei Shoka <xrdsby@gmail.com>
2022-10-07 17:07:03 +0300

    try catch implementation changed

--- app/src/main/java/com/example/myapplication/network/GetTodoListRunnable.java
+++ app/src/main/java/com/example/myapplication/network/GetTodoListRunnable.java
@@ -5,8 +5,6 @@ import com.example.myapplication.Todo;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.List;
 
@@ -15,56 +13,24 @@ public class GetTodoListRunnable implements Runnable {
     private static final String FIREBASE_URL = "https://todoapp-f8a0e-default-rtdb.europe-west1.firebasedatabase.app/TodoList/.json";
     private static final String REQUEST_GET = "GET";
     private final TodoJsonReader todoJsonReader = new TodoJsonReader();
-    private InputStream inputStream = null;
 
     @Override
     public void run() {
-
-        URL url = null;
-
         try {
-            url = new URL(FIREBASE_URL);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        HttpURLConnection httpURLConnection = null;
-
-        try {
-            if (url != null) {
-                httpURLConnection = (HttpURLConnection) url.openConnection();
+            URL url = new URL(FIREBASE_URL);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod(REQUEST_GET);
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.connect();
+            if (HttpURLConnection.HTTP_OK == httpURLConnection.getResponseCode()) {
+                InputStream inputStream = httpURLConnection.getInputStream();
+                List<Todo> todoList = todoJsonReader.readJsonStream(inputStream); // пока сохраняет список тудушек сюда
+                inputStream.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        if (httpURLConnection != null) {
-            try {
-                httpURLConnection.setRequestMethod(REQUEST_GET);
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            }
-            httpURLConnection.setDoInput(true);
-            try {
-                httpURLConnection.connect();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                if (HttpURLConnection.HTTP_OK == httpURLConnection.getResponseCode()) {
-                    inputStream = httpURLConnection.getInputStream();
-                    List<Todo> todoList = todoJsonReader.readJsonStream(inputStream); // пока сохраняет список тудушек сюда
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 }
+
+
